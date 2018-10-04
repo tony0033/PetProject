403a67f65a8ee8bde7cfbc435861574d70516421
이승재 <@LAPTOP-21TFDD8C>
2018-10-04 16:44:45 +0900

    ss

--- FinalProjectPet3/src/main/java/com/itbank/mvc3/InsertController.java
+++ FinalProjectPet3/src/main/java/com/itbank/mvc3/InsertController.java
@@ -1,5 +1,7 @@
 package com.itbank.mvc3;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +16,9 @@ public class InsertController {
 	DiaryDAO diarydao;
 	
 	@RequestMapping("insert.do")
-	public String insert( MemberDTO memberDTO) {
+	public String insert( MemberDTO memberDTO, HttpServletRequest request) {
+		String address2 = request.getParameter("address2");
+		memberDTO.setAddress(memberDTO.getAddress() + " " + address2);
 		dao.insert(memberDTO);
 		return "redirect:main.jsp";
 	}
